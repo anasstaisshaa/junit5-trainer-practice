fc462505a59f55df112397b015963537965b290b
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-03-10 12:05:12 +0200

    add entity

---
+++ src/main/java/edu/AnastasiiaTkachuk/entity/Role.java
@@ -0,0 +1,19 @@
+package edu.AnastasiiaTkachuk.entity;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum Role {
+    USER,
+    ADMIN;
+
+    public static Role find(String role) {
+        return findOpt(role).orElseThrow();
+    }
+
+    public static Optional<Role> findOpt(String role) {
+        return Arrays.stream(values())
+                .filter(it -> it.name().equals(role))
+                .findFirst();
+    }
+}
---
+++ src/main/java/edu/AnastasiiaTkachuk/entity/User.java
@@ -0,0 +1,24 @@
+package edu.AnastasiiaTkachuk.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDate;
+
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    private Integer id;
+    private String name;
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private Role role;
+    private Gender gender;
+}
